cd73f1df5f27f29be468372f629de769ae046a5d
Daniel Schmitz <danieljfa@gmail.com>
2022-06-24 11:30:05 -0300

    Operador logico &&

--- src/components/button.js
+++ src/components/button.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Button(props) {
 
-    const small = props.small ? " is-small " : ""
-    const large = props.large ? " is-large " : ""
-    const danger = props.danger ? " is-danger " : ""
+    const small = props.small && " is-small "
+    const large = props.large && " is-large "
+    const danger = props.danger && " is-danger "
 
     const className = "button is-primary " + small + large + danger
 
