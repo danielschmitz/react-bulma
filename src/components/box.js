4e72fe5646c1b64e96148ad59b77421cdff7973d
Daniel Schmitz <danieljfa@gmail.com>
2022-06-24 12:00:21 -0300

    Box as Component

--- src/components/box.js
+++ src/components/box.js
@@ -1,7 +1,23 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Box(props) {
-    return (
-        <div className="box">{props.children}</div>
-    )
+// export default function Box(props) {
+//     return (
+//         <div className="box">{props.children}</div>
+//     )
+// }
+
+
+import React, { Component } from 'react'
+
+export default class Box extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="box">{this.props.children}</div>
+        )
+    }
 }
